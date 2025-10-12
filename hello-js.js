6f1b25038410b1b03fce3516d48fc8c67b925fd3
dania782002-ai <dania782002@gmail.com>
2025-10-12 14:12:59 +0300

    hello-js.js

---
+++ hello-js.js
@@ -0,0 +1,31 @@
+let name = "danya";
+let age = 23;
+let isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student?", isStudent);
+
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are underage.");
+}
+
+function greet(user) {
+  console.log("Hello, " + user + "!");
+}
+
+greet(name);
+
+let skills = ["HTML", "CSS", "JavaScript"];
+console.log("Main skill:", skills[2]);
+
+
+let profile = {
+  name: name,
+  age: age,
+  student: isStudent
+};
+
+console.log("Profile name is:", profile.name);
