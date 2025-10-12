eb4059739df12e1f3d557284031199e9d0a8043a
dania782002-ai <dania782002@gmail.com>
2025-10-12 12:43:12 +0300

    async-demo.js

---
+++ async-demo.js
@@ -0,0 +1,29 @@
+function fakeFetch() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
+      if (success) {
+        resolve("Data fetched successfully");
+      } else {
+        reject("An error occurred while fetching data");
+      }
+    }, 2000);
+  });
+}
+fakeFetch()
+  .then(result => {
+    console.log("Success:", result);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+  async function fetchData() {
+  try {
+    const result = await fakeFetch();
+    console.log("Success:", result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+fetchData();
