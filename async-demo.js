972b7679142151714c3eb1441cf23b3b820dea54
dania782002-ai <dania782002@gmail.com>
2025-10-14 13:05:24 +0300

    eslint.config.js

--- async-demo.js
+++ async-demo.js
@@ -3,26 +3,26 @@ function fakeFetch() {
     setTimeout(() => {
       const success = true;
       if (success) {
-        resolve("Data fetched successfully");
+        resolve('Data fetched successfully');
       } else {
-        reject("An error occurred while fetching data");
+        reject('An error occurred while fetching data');
       }
     }, 2000);
   });
 }
 fakeFetch()
-  .then(result => {
-    console.log("Success:", result);
+  .then((result) => {
+    console.log('Success:', result);
   })
-  .catch(error => {
-    console.error("Error:", error);
+  .catch((error) => {
+    console.error('Error:', error);
   });
-  async function fetchData() {
+async function fetchData() {
   try {
     const result = await fakeFetch();
-    console.log("Success:", result);
+    console.log('Success:', result);
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 }
 
--- hello-js.js
+++ hello-js.js
@@ -1,31 +1,30 @@
-let name = "danya";
+let name = 'danya';
 let age = 23;
 let isStudent = true;
 
-console.log("Name:", name);
-console.log("Age:", age);
-console.log("Student?", isStudent);
+console.log('Name:', name);
+console.log('Age:', age);
+console.log('Student?', isStudent);
 
 if (age >= 18) {
-  console.log("You are an adult.");
+  console.log('You are an adult.');
 } else {
-  console.log("You are underage.");
+  console.log('You are underage.');
 }
 
 function greet(user) {
-  console.log("Hello, " + user + "!");
+  console.log('Hello, ' + user + '!');
 }
 
 greet(name);
 
-let skills = ["HTML", "CSS", "JavaScript"];
-console.log("Main skill:", skills[2]);
-
+let skills = ['HTML', 'CSS', 'JavaScript'];
+console.log('Main skill:', skills[2]);
 
 let profile = {
   name: name,
   age: age,
-  student: isStudent
+  student: isStudent,
 };
 
-console.log("Profile name is:", profile.name);
+console.log('Profile name is:', profile.name);
